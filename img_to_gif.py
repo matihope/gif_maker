09c55bc86429d4c0aab5fee706d77665d415c211
Mateusz Kolpa <mateusz.kolpa@wp.pl>
2019-11-29 23:49:52 +0100

    Few Changes

--- img_to_gif.py
+++ img_to_gif.py
@@ -9,8 +9,9 @@ from PIL import Image
 def get_args(argv):
     input_dir = ''
     output_file = ''
+    frame_time = 1000
 
-    opts, args = getopt.getopt(argv, "hi:o:")
+    opts, args = getopt.getopt(argv, "hi:o:f:")
 
     for opt, arg in opts:
         if opt == '-h':
@@ -21,12 +22,14 @@ def get_args(argv):
             input_dir = arg
         elif opt == "-o":
             output_file = arg + '.gif' if '.gif' not in arg else ''
+        elif opt == '-f':
+            frame_time = int(arg)
 
-    return input_dir, output_file
+    return input_dir, output_file, frame_time
 
 
-def main(input_dir, output_name, fps=50):
-    path = os.path.abspath(input_dir)
+def main():
+    path = os.path.abspath(input_directory)
     files = []
     images = []
     # r=root, d=directories, f = files
@@ -34,21 +37,21 @@ def main(input_dir, output_name, fps=50):
         for file in f:
             if any(x in file for x in ['.png', '.bmp', '.jpg', '.jpeg']):
                 files.append(os.path.join(r, file))
-    files.sort(key=lambda x: x.split('.')[0])
+    files.sort()
 
     for file in files:
         images.append(Image.open(file))
     images[0].save(output_file_name, save_all=True, append_images=images[1:], optimize=False,
-                   duration=len(images)*fps, loop=0)
+                   duration=frame_duration, loop=0)
 
-    print(f'{output_name} was successfully created!')
-    print(f'Directory:{os.path.abspath(output_name)}')
+    print(f'{output_file_name} was successfully created!')
+    print(f'Directory:{os.path.abspath(output_file_name)}')
 
 
 if __name__ == '__main__':
-    input_directory, output_file_name = get_args(sys.argv[1:])
+    input_directory, output_file_name, frame_duration = get_args(sys.argv[1:])
     if os.path.exists(input_directory):
-        main(input_directory, output_file_name)
+        main()
     else:
         print(f'{os.path.basename(__file__)} -i <input_directory> -o <output_file>')
         print(f'Example: {os.path.basename(__file__)} -i inputs -o test.gif')
