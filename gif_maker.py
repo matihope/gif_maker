8402a47a22634b3cd5672a91fd47b6a29b7f15d4
Mateusz Kolpa <mateusz.kolpa@wp.pl>
2020-01-12 15:39:41 +0100

    Update 1.1, added transparent gif support

--- gif_maker.py
+++ gif_maker.py
@@ -6,7 +6,26 @@ import os
 from PIL import Image
 
 
-def create_gif():
+def gen_transparent_frame(path):
+    im = Image.open(path)
+    alpha = im.getchannel('A')
+
+    # Convert the image into P mode but only use 255 colors in the palette out of 256
+    im = im.convert('RGB').convert('P', palette=Image.ADAPTIVE, colors=255)
+
+    # Set all pixel values below 128 to 255 , and the rest to 0
+    mask = Image.eval(alpha, lambda a: 255 if a <= 128 else 0)
+
+    # Paste the color of index 255 and use alpha as a mask
+    im.paste(255, mask)
+
+    # The transparency index is 255
+    im.info['transparency'] = 255
+
+    return im
+
+
+def create_gif(transparent):
     path = os.path.abspath(input_directory)
     files = []
     images = []
@@ -23,11 +42,15 @@ def create_gif():
     files.sort(key=lambda x: int(x.split('.')[0]))
 
     for file in files:
-        file = os.path.join(path, file)
-        images.append(Image.open(file))
+        file_path = os.path.join(path, file)
 
-    images[0].save(output_file, save_all=True, append_images=images[1:], optimize=False,
-                   duration=frame_time, loop=0)
+        if transparent:
+            images.append(gen_transparent_frame(file_path))
+        else:
+            images.append(Image.open(file_path))
+
+    images[0].save(output_file, save_all=True, append_images=images[1:], duration=frame_time,
+                   loop=0, optimize=False, disposal=2)
 
     print(f'{output_file} was successfully created!')
     print(f'Directory:{os.path.abspath(output_file)}')
@@ -35,7 +58,11 @@ def create_gif():
 
 def help_me():
     print('--------------------------------------------')
-    print(f'{os.path.basename(__file__)} -i <input_directory> -o <output_file.gif> -f <frame_time_in_milliseconds>')
+    print(f'{os.path.basename(__file__)}:')
+    print(f'    -i <input_directory> : The directory with input images (default: {input_directory})')
+    print(f'    -o <output_file.gif> : Name of the output file (default: {output_file})')
+    print(f'    -f <frame_time> : Frame duration in milliseconds (default: {frame_time})')
+    print(f'    -t : Gif will have an alpha channel (default: NO TAG)')
     print()
     print(f'Defaults and Example:\n   > python3 {os.path.basename(__file__)} -i inputs -o test.gif -f 200')
     print()
@@ -46,21 +73,24 @@ def help_me():
 
 if __name__ == '__main__':
     input_directory = 'inputs'
-    output_file = 'test.gif'
+    output_file = 'animation.gif'
     frame_time = 200
+    transparent = False
 
-    opts, args = getopt.getopt(sys.argv[1:], "hi:o:f:")
+    opts, args = getopt.getopt(sys.argv[1:], "hi:o:f:t")
     for opt, arg in opts:
         if opt == '-h':
             help_me()
         elif opt == "-i":
             input_dir = arg
         elif opt == "-o":
-            output_file = arg + '.gif' if '.gif' not in arg else ''
+            output_file = arg + '.gif' if '.gif' not in arg else arg
         elif opt == '-f':
             frame_time = int(arg)
+        elif opt == '-t':
+            transparent = True
 
     if os.path.exists(input_directory):
-        create_gif()
+        create_gif(transparent)
     else:
         help_me()
